55f393e3f91f0e95f1a71de414bf1d024653c215
yundogi <lvbhkwak@gmail.com>
2024-05-29 15:20:41 +0900

    add image

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import re_path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
   re_path(r'^$', views.post_list, name='post_list'),
@@ -7,3 +9,6 @@ urlpatterns = [
   re_path(r'^post/new/$', views.post_new, name='post_new'),
   re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
